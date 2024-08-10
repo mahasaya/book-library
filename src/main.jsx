dcd8666d53651521f55df6fff6262e0472978b1d
mahasaya <83829233+mahasaya@users.noreply.github.com>
2024-08-10 20:31:42 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { Toaster } from "react-hot-toast";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/book-library/">
     <Provider store={store}>
       <App />
       <Toaster />
